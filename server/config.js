3efc8ca47bcd76a7ac3ec3f3c891fdc19f80c234
zenphia <93544652+zephiii@users.noreply.github.com>
2025-12-23 12:37:35 +0000

    one default server only for now

--- server/config.js
+++ server/config.js
@@ -11,7 +11,7 @@ module.exports = {
 
     servers: [ // Make sure to change the HOST, PORT and SERVER_ID between servers!
         {
-            load_on_mainserver: true, // Only one server at a time can have this enabled.
+            load_on_mainserver: false, // Only one server at a time can have this enabled.
             // The above is required if your VM (the machine that hosts the website stuff) doesn't support multi-ports and forces everything through the main server.
             // This also overrides the below HOST and PORT settings to be identical to the main server's HOST/PORT (by default, 3000).
 
@@ -21,26 +21,7 @@ module.exports = {
             featured: false,
 
             region: "local", // The region the server is on.
-            gamemode: ["tdm"], // The selected gamemode.
-            player_cap: 80, // Not including bots. Set to 0 to disable.
-
-            properties: { // This overrides settings in the config.js file, assuming the selected gamemode doesn't also override it.
-                teams: 2,
-                bot_cap: 32,
-            }
-        },
-        {
-            load_on_mainserver: false, // Only one server at a time can have this enabled.
-            // The above is required if your VM (the machine that hosts the website stuff) doesn't support multi-ports and forces everything through the main server.
-            // This also overrides the below HOST and PORT settings to be identical to the main server's HOST/PORT (by default, 3000).
-
-            host: "localhost:3002", // Server host location.
-            port: 3002, // The port on the server.
-            id: "lod", // (<HOST>/#<SERVER_ID>)
-            featured: false,
-
-            region: "local", // The region the server is on.
-            gamemode: ["ffa"], // The selected gamemode.
+            gamemode: ["sandbox"], // The selected gamemode.
             player_cap: 80, // Not including bots. Set to 0 to disable.
 
             properties: { // This overrides settings in the config.js file, assuming the selected gamemode doesn't also override it.
@@ -65,24 +46,6 @@ module.exports = {
                 },
             }
         },
-        {
-            load_on_mainserver: false, // Only one server at a time can have this enabled.
-            // The above is required if your VM (the machine that hosts the website stuff) doesn't support multi-ports and forces everything through the main server.
-            // This also overrides the below HOST and PORT settings to be identical to the main server's HOST/PORT (by default, 3000).
-
-            host: "localhost:3003", // Server host location.
-            port: 3003, // The port on the server.
-            id: "loe", // (<HOST>/#<SERVER_ID>)
-            featured: false,
-
-            region: "local", // The region the server is on.
-            gamemode: ["sandbox"], // The selected gamemode.
-            player_cap: 0, // Not including bots. Set to 0 to disable.
-
-            properties: { // This overrides settings in the config.js file, assuming the selected gamemode doesn't also override it.
-                bot_cap: 0,
-            }
-        }
     ],
 
     // Web Server
